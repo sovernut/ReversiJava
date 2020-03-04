ea71e3d06eac66582e7a9fa20c8563fb76211f44
Thanut Suwannawong <thanut@sirisoft.co.th>
2020-03-04 16:32:46 +0700

    [Implement]:[Reversi]:add logic

--- Reversi.java
+++ Reversi.java
@@ -15,7 +15,7 @@ class Reversi {
         board.place(Constant.BLACK_NAME, 5, 5);
         board.place(Constant.WHITE_NAME, 4, 5);
         board.place(Constant.WHITE_NAME, 5, 4);
-        board.setMarker(generateMarkerForTurn(getTurnName())); 
+        board.setMarker(generateMarkerForTurn(getTurnName()));
         board.show();
         try {
             while (!gameEnd) {
@@ -29,13 +29,13 @@ class Reversi {
                 if (row == 99)
                     gameEnd = true;
                 String[][] m = generateMarkerForTurn(getTurnName());
-                board.setMarker(m); 
+                board.setMarker(m);
                 board.show();
                 countScore();
             }
         } catch (Exception e) {
             System.out.println("Invalid arguments.");
-            e.printStackTrace(); 
+            e.printStackTrace();
         } finally {
             in.close();
         }
@@ -46,7 +46,7 @@ class Reversi {
         return isWhiteTurn ? Constant.WHITE_NAME : Constant.BLACK_NAME;
     }
 
-    static String getEnemyTurnName(String turn){
+    static String getEnemyTurnName(String turn) {
         return turn == Constant.WHITE_NAME ? Constant.BLACK_NAME : Constant.WHITE_NAME;
     }
 
@@ -82,166 +82,215 @@ class Reversi {
         int col = c - 1;
         Piece[][] boards = board.getBoard();
         try {
-
-            //if (((row - 1) >= 0 && (col - 1) >= 0) && ((row + 1) <= 7 && (col + 1) <= 7)) {
-                /*
-                 * x x x 
-                 * - o - 
-                 * - - -
-                 */
-                if ((row>0 && col>0)&&boards[row - 1][col - 1] != null)
-                    checkNextPiece(row, col, Constant.UPPER_LEFT);
-                if ((row>0)&&boards[row - 1][col] != null)
-                    checkNextPiece(row, col, Constant.UPPER_MID);
-                if ((row>0 && col<7)&&boards[row - 1][col + 1] != null)
-                    checkNextPiece(row, col, Constant.UPPER_RIGHT);
-                /*
-                 * - - - 
-                 * x o x 
-                 * - - -
-                 */
-                if ((col>0)&&boards[row][col - 1] != null)
-                    checkNextPiece(row, col, Constant.MIDDLE_LEFT);
-                if ((col<7)&&boards[row][col + 1] != null)
-                    checkNextPiece(row, col, Constant.MIDDLE_RIGHT);
-                /*
-                 * - - - 
-                 * - o - 
-                 * x x x
-                 */
-                if ((row<7&&col>0)&&boards[row + 1][col - 1] != null)
-                    checkNextPiece(row, col, Constant.LOWER_LEFT);
-                if ((row<7)&&boards[row + 1][col] != null)
-                    checkNextPiece(row, col, Constant.LOWER_MID);
-                if ((row<7&&col<7)&&boards[row + 1][col + 1] != null)
-                    checkNextPiece(row, col, Constant.LOWER_RIGHT);
-           // }
-            // System.out.println("Existing around pieces :: " + existingPiece);
-
+            int[] indexX = { -1, -1, -1, 0, 0, 1, 1, 1 };
+            int[] indexY = { -1, 0, 1, -1, 1, -1, 0, 1 };
+            for (int i = 0; i < 8; i++) {
+                int indexXWithDir = row + indexX[i];
+                int indexYWithDir = col + indexY[i];
+                // System.out.println("indexXWithDir :: "+indexXWithDir);
+                // System.out.println("indexYWithDir :: "+indexYWithDir);
+                if ((indexXWithDir < 0 || indexXWithDir > 7) || (indexYWithDir > 7 || indexYWithDir < 0)) {
+                    System.out.println("CONTINUE");
+                    continue;
+                }
+                // System.out.println("boards[indexXWithDir][indexYWithDir].getName() ::: "
+                // + boards[indexXWithDir][indexYWithDir].getName());
+                // System.out.println("getTurnName() ::: " + getTurnName());
+                if (boards[indexXWithDir][indexYWithDir] != null
+                        && boards[indexXWithDir][indexYWithDir].getName() != getTurnName()) {
+                    System.out.println("Existing!!!");
+                    checkNextPiece(indexXWithDir, indexYWithDir, i);
+                }
+            }
         } catch (Exception e) {
-            System.out.println("Error :: " + e);
+            System.out.println("Conquer has errored :: " + e);
         }
     }
 
-    static void checkNextPiece(int row, int col, String direction) {
+    static void checkNextPiece(int row, int col, int direction) {
         Piece[][] boards = board.getBoard();
+        int count = 0;
         switch (direction) {
-            case Constant.UPPER_LEFT:
-                System.out.println(Constant.UPPER_LEFT);
-                if (isSameColor(boards[row - 1][col - 1])) {
-                    break;
-                } else {
-                    System.out.println(" UPPER_LEFT CAN EAT !!");
+            // UPPER LEFT
+            case 0:
+                System.out.println("UPPER LEFT");
+                while (true) {
+                    if (boards[row][col] == null) {
+                        break;
+                    } else if (boards[row][col].getName() == getTurnName()) {
+                        System.out.println("CASE UPPER LEFT FOUND");
+                        for (int j = count; j > 0; j--) {
+                            row++;
+                            col++;
+                            boards[row][col].setNameAndSymbol(getTurnName());
+                        }
+                        break;
+                    } else if (row == 0 || row == 7 || col == 0 || col == 8) {
+                        break;
+                    } else {
+                        count++;
+                    }
+                    row--;
+                    col--;
                 }
                 break;
-            case Constant.UPPER_MID:
-                System.out.println(Constant.UPPER_MID);
-                if (isSameColor(boards[row - 1][col])) {
-                    break;
-                } else {
-                    System.out.println("UPPER_MID CAN EAT !!");
-                    int count = 0;
-                    for (int i = row - 2; i >= 0; i--) {
-                        count++;
-                        if (isSameColor(boards[i][col])) {
-                            for (int j = i + 1; count > 0; count--) {
-                                boards[j][col].setNameAndSymbol(getTurnName());
-                                j++;
-                            }
-                            break;
+            // UPPER MID
+            case 1:
+                System.out.println("CASE UPPER MID");
+                while (true) {
+                    if (boards[row][col] == null) {
+                        break;
+                    } else if (boards[row][col].getName() == getTurnName()) {
+                        System.out.println("CASE UPPER MID FOUND");
+                        for (int j = count; j > 0; j--) {
+                            row++;
+                            boards[row][col].setNameAndSymbol(getTurnName());
                         }
+                        break;
+                    } else if (row == 0 || row == 7) {
+                        break;
+                    } else {
+                        count++;
                     }
-                    break;
-                }
-            case Constant.UPPER_RIGHT:
-                System.out.println(Constant.UPPER_RIGHT);
-                if (isSameColor(boards[row - 1][col + 1])) {
-
-                } else {
-                    System.out.println("UPPER_RIGHT CAN EAT !!");
+                    row--;
                 }
                 break;
-            case Constant.MIDDLE_LEFT:
-                System.out.println(Constant.MIDDLE_LEFT);
-                if (isSameColor(boards[row][col - 1])) {
-                    break;
-                } else {
-                    System.out.println("MIDDLE_LEFT CAN EAT !!");
-                    int count = 0;
-                    for (int i = col - 2; i >= 0; i--) {
-                        count++;
-                        if (isSameColor(boards[row][i])) {
-                            // System.out.println("count :: "+count);
-                            // System.out.println("boards[row][i] :: ["+row+"]["+i+"]");
-                            // System.out.println("EAT THEM ALLLLL!");
-                            for (int j = i + 1; count > 0; count--) {
-                                boards[row][j].setNameAndSymbol(getTurnName());
-                                j++;
-                            }
-                            break;
+            // UPPER RIGHT
+            case 2:
+                System.out.println("UPPER RIGHT");
+                while (true) {
+                    if (boards[row][col] == null) {
+                        break;
+                    } else if (boards[row][col].getName() == getTurnName()) {
+                        System.out.println("CASE UPPER RIGHT FOUND");
+                        for (int j = count; j > 0; j--) {
+                            row++;
+                            col--;
+                            boards[row][col].setNameAndSymbol(getTurnName());
                         }
-                    }
-                    break;
-                }
-
-            case Constant.MIDDLE_RIGHT:
-                System.out.println(Constant.MIDDLE_RIGHT);
-                if (isSameColor(boards[row][col + 1])) {
-                    break;
-                } else {
-                    System.out.println("MIDDLE_RIGHT CAN EAT !!");
-                    int count = 0;
-                    for (int i = col + 2; i <= 7; i++) {
+                        break;
+                    } else if (row == 0 || row == 7 || col == 0 || col == 8) {
+                        break;
+                    } else {
                         count++;
-                        if (isSameColor(boards[row][i])) {
-                            for (int j = i - 1; count > 0; count--) {
-                                boards[row][j].setNameAndSymbol(getTurnName());
-                                j--;
-                            }
-                            break;
-                        }
                     }
-                    break;
-                }
-            case Constant.LOWER_LEFT:
-                System.out.println(Constant.LOWER_LEFT);
-                if (isSameColor(boards[row + 1][col - 1])) {
-
-                } else {
-                    System.out.println("LOWER_LEFT CAN EAT !!");
+                    row--;
+                    col++;
                 }
                 break;
-            case Constant.LOWER_MID:
-                System.out.println(Constant.LOWER_MID);
-                if (isSameColor(boards[row + 1][col])) {
-
-                } else {
-                    System.out.println("LOWER_MID CAN EAT !!");
-                    int count = 0;
-                    for (int i = row + 2; i >= 0; i--) {
-                        count++;
-                        if (isSameColor(boards[i][col])) {
-                            for (int j = i + 1; count > 0; count--) {
-                                boards[j][col].setNameAndSymbol(getTurnName());
-                                j++;
-                            }
-                            break;
+            // MIDDLE LEFT
+            case 3:
+                System.out.println("CASE MIDDLE LEFT");
+                while (true) {
+                    if (boards[row][col] == null) {
+                        break;
+                    } else if (boards[row][col].getName() == getTurnName()) {
+                        System.out.println("CASE MIDDLE LEFT FOUND");
+                        for (int j = count; j > 0; j--) {
+                            col++;
+                            boards[row][col].setNameAndSymbol(getTurnName());
                         }
+                        break;
+                    } else if (col == 0 || col == 7) {
+                        break;
+                    } else {
+                        count++;
                     }
-                    break;
+                    col--;
                 }
                 break;
-            case Constant.LOWER_RIGHT:
-                System.out.println(Constant.LOWER_RIGHT);
-                if (isSameColor(boards[row + 1][col + 1])) {
-                    break;
-                } else {
-                    System.out.println("LOWER_RIGHT CAN EAT !!");
+            // MIDDLE RIGHT
+            case 4:
+                System.out.println("CASE MIDDLE RIGHT");
+                while (true) {
+                    if (boards[row][col] == null) {
+                        break;
+                    } else if (boards[row][col].getName() == getTurnName()) {
+                        System.out.println("CASE MIDDLE RIGHT FOUND");
+                        for (int j = count; j > 0; j--) {
+                            col--;
+                            boards[row][col].setNameAndSymbol(getTurnName());
+                        }
+                        break;
+                    } else if (col == 0 || col == 7) {
+                        break;
+                    } else {
+                        count++;
+                    }
+                    col++;
+                }
+                break;
+            // LOWER LEFT
+            case 5:
+                System.out.println("LOWER LEFT");
+                while (true) {
+                    if (boards[row][col] == null) {
+                        break;
+                    } else if (boards[row][col].getName() == getTurnName()) {
+                        System.out.println("CASE LOWER LEFT FOUND");
+                        for (int j = count; j > 0; j--) {
+                            row--;
+                            col++;
+                            boards[row][col].setNameAndSymbol(getTurnName());
+                        }
+                        break;
+                    } else if (row == 0 || row == 7 || col == 0 || col == 8) {
+                        break;
+                    } else {
+                        count++;
+                    }
+                    row++;
+                    col--;
+                }
+                break;
+            // LOWER MID
+            case 6:
+                System.out.println("LOWER MID");
+                while (true) {
+                    if (boards[row][col] == null) {
+                        break;
+                    } else if (boards[row][col].getName() == getTurnName()) {
+                        System.out.println("CASE LOWER MID FOUND");
+                        for (int j = count; j > 0; j--) {
+                            row--;
+                            boards[row][col].setNameAndSymbol(getTurnName());
+                        }
+                        break;
+                    } else if (row == 0 || row == 7) {
+                        break;
+                    } else {
+                        count++;
+                    }
+                    row++;
+                }
+                break;
+            // LOWER RIGHT
+            case 7:
+                System.out.println("LOWER RIGHT");
+                while (true) {
+                    if (boards[row][col] == null) {
+                        break;
+                    } else if (boards[row][col].getName() == getTurnName()) {
+                        System.out.println("CASE LOWER RIGHT FOUND");
+                        for (int j = count; j > 0; j--) {
+                            row--;
+                            col--;
+                            boards[row][col].setNameAndSymbol(getTurnName());
+                        }
+                        break;
+                    } else if (row == 0 || row == 7 || col == 0 || col == 8) {
+                        break;
+                    } else {
+                        count++;
+                    }
+                    row++;
+                    col++;
                 }
                 break;
             default:
                 break;
         }
+
     }
 
     static boolean isSameColor(Piece board) {
@@ -252,9 +301,9 @@ class Reversi {
         String[][] marker = new String[Constant.BOARD_SIZE][Constant.BOARD_SIZE];
         Piece[][] c_board = board.getBoard();
         String enemy = getEnemyTurnName(turn);
-        for(int i = 0;i < Constant.BOARD_SIZE; i++){
-            for(int k = 0; k < Constant.BOARD_SIZE;k++){        
-                if (c_board[i][k] != null && c_board[i][k].getName().equals(enemy)){
+        for (int i = 0; i < Constant.BOARD_SIZE; i++) {
+            for (int k = 0; k < Constant.BOARD_SIZE; k++) {
+                if (c_board[i][k] != null && c_board[i][k].getName().equals(enemy)) {
                     markPlaceable(marker, turn, i, k);
                 }
             }
@@ -262,13 +311,15 @@ class Reversi {
         return marker;
     }
 
-    static void markPlaceable(String[][] marker, String turn, int x_p, int y_p){
-        int x[] = {-1,-1,-1, 0, 0, 1, 1, 1};
-        int y[] = {-1, 0, 1,-1, 1,-1, 0, 1};
-        for (int i = 0;i < x.length;i++){
+    static void markPlaceable(String[][] marker, String turn, int x_p, int y_p) {
+        int x[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+        int y[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+        for (int i = 0; i < x.length; i++) {
             boolean isInBoard = board.isInBoard(x[i] + x_p, y[i] + y_p);
-            boolean isAllyPieceOpposite = isAllyPieceOpposite(turn, x_p + x[i] * -1, y_p + y[i] * -1, x[i] * -1, y[i] * -1);
-            // System.out.println("========" + (x[i] + x_p + 1) + " " + (y[i] + y_p + 1) + "===============");
+            boolean isAllyPieceOpposite = isAllyPieceOpposite(turn, x_p + x[i] * -1, y_p + y[i] * -1, x[i] * -1,
+                    y[i] * -1);
+            // System.out.println("========" + (x[i] + x_p + 1) + " " + (y[i] + y_p + 1) +
+            // "===============");
             // System.out.println("isINBOARD "+isInBoard);
             // System.out.println("isAllyPieceOpposite "+isAllyPieceOpposite);
             if (isInBoard && isAllyPieceOpposite && !board.isCellPlaced(x[i] + x_p, y[i] + y_p)) {
@@ -277,10 +328,10 @@ class Reversi {
         }
     }
 
-    static boolean isAllyPieceOpposite(String turn, int x, int y, int x_dir, int y_dir){
+    static boolean isAllyPieceOpposite(String turn, int x, int y, int x_dir, int y_dir) {
         Piece[][] c_board = board.getBoard();
-        while (board.isInBoard(x, y) && board.isCellPlaced(x, y)){
-            if (c_board[x][y].getName().equals(turn)){
+        while (board.isInBoard(x, y) && board.isCellPlaced(x, y)) {
+            if (c_board[x][y].getName().equals(turn)) {
                 return true;
             }
             x += x_dir;
